de8a36c9f32560298c5e203c05dfa4d5df440f82
玩火 <niltok@163.com>
2022-10-09 21:50:18 +0800

    finish MyTicket

---
+++ wwwroot/script/FocusTo.js
@@ -0,0 +1,9 @@
+﻿function focusTo(id) {
+    setTimeout(() => {
+        let e = document.getElementById(id)
+        if (e) {
+            e.focus()
+            e.scrollTo()
+        }
+    }, 1000)
+}
